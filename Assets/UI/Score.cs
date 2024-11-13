e9dc2a7ded42d93cb7a818d073120552a95cc0e1
ArTeGaS-2 <probablytorvik@gmail.com>
2024-11-13 19:59:30 +0200

    Main menu and scene managment

--- Assets/UI/Score.cs
+++ Assets/UI/Score.cs
@@ -8,8 +8,13 @@ public class Score : MonoBehaviour
     public static Score Instance;
     public int score = 0;
     public TextMeshProUGUI text;
+    private void Start()
+    {
+        Instance = this;
+    }
     public void AddScore()
     {
         score++;
+        text.text = $"Зібрано:{score}";
     }
 }
