d836893edf85d134e6286b706057652bd1e91144
ArTeGaS-2 <probablytorvik@gmail.com>
2025-01-15 19:57:51 +0200

    Обертання

--- Assets/DynamicDanger/DynamicDanger.cs
+++ Assets/DynamicDanger/DynamicDanger.cs
@@ -8,19 +8,43 @@ public class DynamicDanger : MonoBehaviour
     public float objectSpeed = 5f; // Швидкість ворога / об'єкта
 
     private Rigidbody rb; // Змінна для доступу до компоненту
+
+    private float currentAngleMod = 0f; // Поточний модифікатор напрямку
+    public float anglePerIteration = 90f; // Зміна кута за ітерацію
+
     private void OnTriggerEnter(Collider other) // Перевіряє зіткнення
     {
-        Destroy(other.gameObject); // Знищує об'єкт з яким зіткнувся
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject); // Знищує об'єкт з яким зіткнувся
+        }
     }
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); // Прив'язуємо Rigidbody до змінної "rb"
+        StartCoroutine(RotateDynamicDanger());
     }
     private void Update()
     {
         // Прискоює об'єкт у напрямку
-        rb.velocity = Vector3.forward * // Напрямок - вперед(forward)
+        rb.velocity = Vector3.left * // Напрямок - вперед(forward)
                       objectSpeed * // Швидкість
                       Time.deltaTime; // Узгодження ігрового часу
+    }
+    private IEnumerator RotateDynamicDanger()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(needToGo); // затримка у секундах
+            // Зміна поточного кута
+            
+            Vector3 currenEulerAngles = transform.rotation.eulerAngles;
+
+            currenEulerAngles.z += currentAngleMod;
+
+            transform.rotation = Quaternion.Euler(currenEulerAngles);
+
+            currentAngleMod += anglePerIteration; // Зміна значення модифікатора
+        }
     }
 }
