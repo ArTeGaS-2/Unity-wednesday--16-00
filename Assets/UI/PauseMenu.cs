f1b8be3df9c43775fdc58ce535a44657d0e1a31f
ArTeGaS-2 <probablytorvik@gmail.com>
2025-05-14 17:55:40 +0300

    Скрипт PauseMenu

--- Assets/UI/PauseMenu.cs
+++ Assets/UI/PauseMenu.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static PauseMenu instance; // Один екземпляр вікна на гру
     public GameObject pauseMenuObject; // Вікно паузи
+    private void Awake()
+    {
+        instance = this; // Прив'язуємо існуюче вікно до змінної
+        pauseMenuObject.SetActive(false); // Вимикаємо вікно на початку
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) // Якщо натиснуто ESC
+        {
+            OpenPauseMenu(); // Відкриваємо меню паузи
+        }
+    }
     public void OpenPauseMenu()
     {
         pauseMenuObject.SetActive(true); // Активація меню паузи
@@ -14,5 +28,10 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenuObject.SetActive(false); // Декативація
         Time.timeScale = 1.0f; // Поновлення часу
+    }
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1.0f; // Повертаємо час
+        SceneManager.LoadScene(0); // Завантажуємо меню (1 в дужках якщо не меню)
     }
 }
